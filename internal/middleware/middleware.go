93f29371c8bb65f25b80c305e96b32968523f3f3
Fábián Álmos <fabian.almos77@gmail.com>
2023-06-18 19:39:57 +0200

    Added request context for auth middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -3,24 +3,34 @@ package middleware
 import (
 	"authJWT/configs"
 	"authJWT/internal/service"
+	"context"
 	"net/http"
 )
 
 type Func func(handler http.Handler) http.Handler
 
+type contextKey string
+
+const (
+	contextClaimKey contextKey = "claims"
+)
+
 func CheckAccessTokenValidity(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ats := configs.NewConfig().AccessTokenSecret
-		authHeader := r.Header.Get("Authorization")
-		tokenString := service.GetTokenFromBearerString(authHeader)
-		claims, err := service.ValidateToken(tokenString, ats)
+		claims, err := service.GetClaims(w, r, ats)
+
 		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
+
+		ctx := context.WithValue(context.Background(), contextClaimKey, claims)
+		rctx := r.WithContext(ctx)
+
 		if claims != nil {
 			w.WriteHeader(http.StatusOK)
-			next.ServeHTTP(w, r)
+			next.ServeHTTP(w, rctx)
 			return
 		}
 	})
